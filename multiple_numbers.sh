a0b78ef56df99d82ef99f21fd8b36edb262841b5
caotritran <caotritran.14@gmail.com>
2018-11-15 12:14:45 +0700

    Create multiple_numbers.sh

---
+++ multiple_numbers.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+#exercise: Boi Chung
+#description:
+#Write a bash program to print out the console the integer numbers from a to b (b >= a, a and b input from keyboard) , 
+#but if that number that is multiples of 7 then print out 'abc', if that number is multiples of 13 then print out 'xyz'. 
+#For those numbers those are multiple of both 7 and 13 then print out 'a-z' .
+
+echo -n "input 2 numbers, b >= a: "
+read a b
+
+if [ $b -ge $a ]; then
+        for ((i=$a; i<=$b; i++ )); do
+                if [ $(($i % 7)) -eq 0 ]; then
+					echo -n "abc "
+                elif [ $(($i % 13)) -eq 0 ]; then
+					echo -n "xyz "
+                elif [ $(($i % 7)) -eq 0 ] && [ $(($i % 13)) -eq 0 ]; then
+					echo -n "abcxyz "
+				else
+					echo -n "$i "
+                fi
+        done
+else
+        echo "b=$b < a=$a, wrong!"
+        exit 1
+fi
